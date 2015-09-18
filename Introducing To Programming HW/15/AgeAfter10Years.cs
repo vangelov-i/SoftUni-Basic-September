d6c2f58f5901228b5f1c4362e439fcb3b0a28cab
vangelov-i <iliyan_vangelov@abv.bg>
2015-09-18 20:19:50 +0300

    new version

---
+++ Introducing To Programming HW/15/AgeAfter10Years.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace P15AgeAfter10Years
+{
+    class AgeAfter10Years
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("bg-BG");
+            Console.Write("Please enter your birth date in format dd/mm/yyy: ");
+            DateTime bDay = DateTime.Parse(Console.ReadLine());
+            int ageNow = (int)((DateTime.Now - bDay).TotalDays / 365.25); // 365.25 because once in every 4 years you have an year that is 366 days long
+            int age10Years = ageNow + 10;
+            Console.WriteLine("You are {0} years old. In 10 years you\'ll be {1}", ageNow, age10Years);
+        }
+    }
+}
